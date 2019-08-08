ed92dd06df5250b2b83f866298948665eddb672f
Ronadlo matheus <matheus.dominguez@hotmail.com>
2019-08-08 15:22:09 +0200

    Tela de login

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \hcodebr\Page;
 use \hcodebr\PageAdmin;
+use \hcodebr\Model\User;
 
 $app = new Slim();
 
@@ -20,9 +21,38 @@ $page->setTpl("index");
 
 $app->get('/admin', function() {
 
+	User::verifyLogin();
+
 $page = new PageAdmin();
 
 $page->setTpl("index");
+
+});
+
+$app->get('/admin/login', function() {
+
+$page = new PageAdmin([
+    "header"=>false,
+    "footer"=>false
+]);
+
+$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function() {
+
+User::login($_POST["login"], $_POST["password"]);
+
+header("Location: /admin");
+exit;
+
+});
+$app->get('/admin/logout', function() {
+
+User::logout();
+header("Location: /admin/login");
+exit;
 
 });
 
--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -8,6 +8,8 @@ class Page{
     private $tpl;
     private $options = [];
     private $defaults = [
+        "header"=>true,
+        "footer"=>true,
         "data"=>[]
     ];
 
@@ -27,8 +29,9 @@ class Page{
     $this->setData($this->options["data"]);
      
     
-    $this->tpl->draw("header");
-
+    if($this->options["header"] === true) {
+        $this->tpl->draw("header");
+     }
 
 	}
     private function setData($data = array()){
@@ -49,8 +52,9 @@ class Page{
 
 	public function __destruct(){
         
+       if($this->options["footer"] === true){ 
         $this->tpl->draw("footer");
-
+       }
 	}
 }
 
